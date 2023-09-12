7e3e97a91b11e3d2affde7de1b0b8d3cb830715d
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-09-12 22:41:08 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,34 @@
+var ms = ThisDrawing.ModelSpace;
+var ut  = ThisDrawing.Utility;
+var db = ThisDrawing.Database
+var layers = db.Layers
+
+ut.Prompt("Начало работы скрипта")
+
+var items_layer_name = "!Светильники"
+var lines_layer_name = "!Метки размеров"
+
+var items_layer
+var lines_layer
+
+var check = 0
+
+for (i=0; i < layers.Count; i++) {
+  if (layers.Item(i).Name == items_layer_name) {
+    item_layer = layers.Item(i)
+    check += 1
+  }
+  if (layers.Item(i).Name == lines_layer_name) {
+    lines_layer = layers.Item(i)
+    check += 1
+  }
+}
+
+if (check < 2) {
+  ut.Prompt("Не найден один из слоев");
+}
+else {
+  ut.Prompt("Оба слоя ("+items_layer_name+" и "+lines_layer_name+") найдены")
+}
+ 
+ 
